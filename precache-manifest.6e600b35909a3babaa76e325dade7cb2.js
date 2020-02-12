446a55b8e8ffba4eb3c58aad02661d621881a53a
sacimenon <sacimenon41@gmail.com>
2020-02-12 22:13:17 +0530

    Updates

---
+++ precache-manifest.6e600b35909a3babaa76e325dade7cb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54da06fe4467b1fb5569fa15c3f5dc3",
+    "url": "/bala-dev/index.html"
+  },
+  {
+    "revision": "771369e4172e272832b7",
+    "url": "/bala-dev/static/css/main.a309d370.chunk.css"
+  },
+  {
+    "revision": "25c86148bd20450d8a63",
+    "url": "/bala-dev/static/js/2.6a9cead3.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/bala-dev/static/js/2.6a9cead3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "771369e4172e272832b7",
+    "url": "/bala-dev/static/js/main.54f91e00.chunk.js"
+  },
+  {
+    "revision": "fd1d31e005d728f2c9bf",
+    "url": "/bala-dev/static/js/runtime-main.685865bf.js"
+  },
+  {
+    "revision": "a95097f73944156f7c1656f518a21f6c",
+    "url": "/bala-dev/static/media/ProfilePic.a95097f7.jpg"
+  },
+  {
+    "revision": "87fa4c272e4711bb7373d96203202e20",
+    "url": "/bala-dev/static/media/bala.87fa4c27.gif"
+  }
+]);
